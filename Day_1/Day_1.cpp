6904e73128fdfdb659a09d2ee913355349374e26
e2 <e2.tejeda@gmail.com>
2018-12-01 20:45:47 -0500

    First commit

---
+++ Day_1/Day_1.cpp
@@ -0,0 +1,21 @@
+// Day_1.cpp Chronal Calibration 
+//For example, if the device displays frequency changes of +1, -2, +3, +1, then starting from a frequency of zero, the following changes would occur:
+//Current frequency  0, change of + 1; resulting frequency  1.
+//Current frequency  1, change of - 2; resulting frequency - 1.
+//Current frequency - 1, change of + 3; resulting frequency  2.
+//Current frequency  2, change of + 1; resulting frequency  3.
+
+#include "pch.h"
+#include <iostream>
+
+void CreateFreqList(char file_name);
+
+int main()
+{
+
+}
+
+void CreateFreqList(char file_name)
+{
+
+}
